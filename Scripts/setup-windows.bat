c2943693ef8d3599b97052032ee9bf04f1fdb0d6
Gabriel Martin <g.martin@htlkrems.at>
2023-09-16 18:36:06 +0200

    Setup Script

---
+++ Scripts/setup-windows.bat
@@ -0,0 +1,8 @@
+﻿ 
+@echo off
+
+winget install Python.Phython.3.10
+winget install Premake.Premake.5.Beta
+winget install Git.Git
+
+python setup.py
---
+++ Scripts/setup.py
@@ -0,0 +1,15 @@
+﻿import os
+import platform
+import subprocess
+
+os.chdir('./../')
+
+print("\nUpdating git submodules ...")
+subprocess.call(["git", "submodule", "update", "--recursive", "--remote"])
+subprocess.call(["git", "submodule", "update", "--init", "--recursive", "--remote"])
+
+print("\nRunning premake...")
+if platform.system() == "Windows":
+    subprocess.call(["Scripts/Dependencies/premake-5.0.0-beta2-windows/premake5.exe", "vs2022"])
+else:
+    subprocess.call(["Scripts/Dependencies/premake-5.0.0-beta2-linux/premake5", "gmake2"])
