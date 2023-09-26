2eb9e363ead5b4182c8b866b13fca60651b377a0
Gabriel Martin <g.martin@htlkrems.at>
2023-09-26 11:45:23 +0200

    Vulkan Sync objects

---
+++ Engine/src/Renderer/Vulkan/VulkanSyncObjects.cpp
@@ -0,0 +1,70 @@
+﻿#include "cepch.h"
+#include "VulkanSyncObjects.h"
+
+#include "VulkanDevice.h"
+#include "Core/Logger.h"
+
+namespace CeltEngine
+{
+    void VulkanFence::Create(VulkanDevice* device, bool isSignaled)
+    {
+        m_Device = device;
+        
+        vk::FenceCreateInfo createInfo;
+        createInfo.flags = isSignaled ? vk::FenceCreateFlagBits::eSignaled : vk::FenceCreateFlags();
+
+        try {
+            m_Fence = device->GetDevice().createFence(createInfo);
+        } catch (const vk::SystemError& err) {
+            CE_ERROR("Failed to create vulkan fence: %s", err.what());
+            CE_BREAK;
+        }
+        CE_TRACE("Created vulkan fence: succesful");
+    }
+
+    void VulkanFence::Destroy() const
+    {
+        m_Device->GetDevice().destroyFence(m_Fence);
+    }
+
+    void VulkanFence::Reset() const
+    {
+        const vk::Result result = m_Device->GetDevice().resetFences(1, &m_Fence);
+        if (result != vk::Result::eSuccess)
+            CE_ERROR("Failed to reset vulkan fence: %s", vk::to_string(result).c_str());
+    }
+
+    void VulkanFence::Wait() const
+    {
+        const vk::Result result = m_Device->GetDevice().waitForFences(1, &m_Fence, VK_TRUE, UINT64_MAX);
+        if (result != vk::Result::eSuccess)
+            CE_ERROR("Failed to wait for vulkan fence: %s", vk::to_string(result).c_str());
+    }
+
+    void VulkanFence::WaitAndReset() const
+    {
+        Wait();
+        Reset();
+    }
+
+    void VulkanSemaphore::Create(VulkanDevice* device)
+    {
+        m_Device = device;
+
+        vk::SemaphoreCreateInfo createInfo;
+        createInfo.flags = vk::SemaphoreCreateFlags();
+        
+        try {
+            m_Semaphore = device->GetDevice().createSemaphore(createInfo);
+        } catch (const vk::SystemError& err) {
+            CE_ERROR("Failed to create vulkan semaphore: %s", err.what());
+            CE_BREAK;
+        }
+        CE_TRACE("Created vulkan semaphore: succesful");
+    }
+
+    void VulkanSemaphore::Destroy() const
+    {
+        m_Device->GetDevice().destroySemaphore(m_Semaphore);
+    }
+}
---
+++ Engine/src/Renderer/Vulkan/VulkanSyncObjects.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+#include <vulkan/vulkan.hpp>
+
+#include "VulkanDevice.h"
+
+namespace CeltEngine
+{
+    class VulkanFence
+    {
+    public:
+        void Create(VulkanDevice* device, bool isSignaled = true);
+        void Destroy() const;
+
+        void Reset() const;
+        void Wait() const;
+        void WaitAndReset() const;
+
+        vk::Fence GetFence() const { return m_Fence; }
+    private:
+        vk::Fence m_Fence;
+        VulkanDevice* m_Device = nullptr;
+    };
+
+    class VulkanSemaphore
+    {
+    public:
+        void Create(VulkanDevice* device);
+        void Destroy() const;
+
+        vk::Semaphore GetSemaphore() const { return m_Semaphore; }
+    private:
+        vk::Semaphore m_Semaphore;
+        VulkanDevice* m_Device = nullptr;
+    };
+}
